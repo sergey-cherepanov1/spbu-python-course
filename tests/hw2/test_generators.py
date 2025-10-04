1ac892fc14a64c6fe61642cbaba9f8ed2ae2b842
kaido <st129987@student.spbu.ru>
2025-10-04 20:28:16 +0300

    pre-commit

--- tests/hw2/test_generators.py
+++ tests/hw2/test_generators.py
@@ -2,29 +2,42 @@ import pytest
 from functools import reduce
 from hw2.generators import generate, convey, unify, collect
 
+
 def custom(data, coeff, degree=2):
     for x in data:
-        yield coeff * x ** degree
+        yield coeff * x**degree
+
 
 @pytest.fixture
 def simple_input():
     return [1, 2, 3, 4]
 
-@pytest.mark.parametrize("inp, expected", [([1, 2 ,3, 4], [1, 2, 3, 4]), ("Hello, World!", ['H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!'])])
+
+@pytest.mark.parametrize(
+    "inp, expected",
+    [
+        ([1, 2, 3, 4], [1, 2, 3, 4]),
+        (
+            "Hello, World!",
+            ["H", "e", "l", "l", "o", ",", " ", "W", "o", "r", "l", "d", "!"],
+        ),
+    ],
+)
 def test_generate(inp, expected):
-    """ Test the generate function with different input types. """
+    """Test the generate function with different input types."""
     assert list(generate(inp)) == expected
 
-@pytest.mark.parametrize("collector,expected", [
-    (list, [1, 2, 3, 4]),
-    (tuple, (1, 2, 3, 4)),
-    (set, {1, 2, 3, 4})
-])
+
+@pytest.mark.parametrize(
+    "collector,expected",
+    [(list, [1, 2, 3, 4]), (tuple, (1, 2, 3, 4)), (set, {1, 2, 3, 4})],
+)
 def test_collect(simple_input, collector, expected):
-    """ Test the collect function with different collection types. """
+    """Test the collect function with different collection types."""
     gen = generate(simple_input)
     result = collect(gen, collector)
     assert result == expected
+
 
 @pytest.mark.parametrize(
     "func,args,kwargs,inp,expected",
@@ -35,14 +48,15 @@ def test_collect(simple_input, collector, expected):
         (enumerate, (), {}, ["a", "b"], [(0, "a"), (1, "b")]),
         (reduce, (lambda x, y: x + y,), {}, [1, 2, 3], [6]),
         (reduce, (lambda x, y: x * y, 10), {}, [1, 2, 3], [60]),
-        (custom, (5,), {"degree": 0.5}, [1, 4, 9], [5, 10, 15])
+        (custom, (5,), {"degree": 0.5}, [1, 4, 9], [5, 10, 15]),
     ],
 )
 def test_unify(func, args, kwargs, inp, expected):
-    """ Test the unify function with various built-in and a custom function. """
+    """Test the unify function with various built-in and a custom function."""
     unified = unify(func, *args, **kwargs)
     out = list(unified(inp))
     assert out == expected
+
 
 @pytest.mark.parametrize(
     "operations,expected",
@@ -50,10 +64,23 @@ def test_unify(func, args, kwargs, inp, expected):
         ([(map, (lambda x: x * 2,), {}), (filter, (lambda x: x > 4,), {})], [6, 8]),
         ([(filter, (lambda x: x < 3,), {})], [1, 2]),
         ([(reduce, (lambda x, y: x + y,), {})], [10]),
-        ([(custom, (3,), {"degree": 2}), (reduce, (lambda x, y: (-x) + y, 5,), {})], [35])
-    ]
+        (
+            [
+                (custom, (3,), {"degree": 2}),
+                (
+                    reduce,
+                    (
+                        lambda x, y: (-x) + y,
+                        5,
+                    ),
+                    {},
+                ),
+            ],
+            [35],
+        ),
+    ],
 )
 def test_convey(simple_input, operations, expected):
-    """ Test the convey function by chaining multiple operations on generated data. """
+    """Test the convey function by chaining multiple operations on generated data."""
     out = convey(generate(simple_input), operations)
     assert list(out) == expected
