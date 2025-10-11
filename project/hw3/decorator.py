cbbfdf438fe4f239f0bac070aaac321ec3697e9f
kaido <st129987@student.spbu.ru>
2025-10-11 21:15:16 +0300

    Fixed minor issues

--- project/hw3/decorator.py
+++ project/hw3/decorator.py
@@ -46,49 +46,59 @@ def smart_args(*, capacity: int = 0, pos_args: bool = False) -> Callable[..., An
             if def_kwargs:
                 for k in def_kwargs:
 
-                    if type(def_kwargs[k]) == Evaluated and def_kwargs[k].func == Isolated:
+                    if (
+                        type(def_kwargs[k]) == Evaluated
+                        and def_kwargs[k].func == Isolated
+                    ):
                         assert False, "Cannot combine Evaluated and Isolated"
-                        
+
                     if type(def_kwargs[k]) == Evaluated and k not in kwargs:
                         kwargs[k] = def_kwargs[k].func()
-                    elif type(def_kwargs[k]) == Isolated and kwargs[k]:
-                        kwargs[k] = copy.deepcopy(kwargs[k])
-                        
-            def_args = spec.defaults
-            if (pos_args and def_args):
-                args = list(args)
-                for a in range(len(def_args)):
-                    index = a + (len(args) - len(def_args))
-                    
-                    if type(def_args[a]) == Evaluated and def_args[a].func == Isolated:
+                    elif type(def_kwargs[k]) == Isolated:
+                        if k in kwargs:
+                            kwargs[k] = copy.deepcopy(kwargs[k])
+                        else:
+                            raise ValueError(
+                                "A keyword argument with default value Isolated was not passed."
+                            )
+            if pos_args:
+                args_l = list(args)
+                spec_args = list(spec.args)
+                spec_defaults = list(spec.defaults) if spec.defaults else []
+                start = len(args_l) - (len(spec_args) - len(spec_defaults))
+
+                for i in range(start, len(spec_defaults)):
+                    if isinstance(spec_defaults[i], Evaluated) and isinstance(
+                        spec_defaults[i].func, Isolated
+                    ):
                         assert False, "Cannot combine Evaluated and Isolated"
-                    
-                    if type(def_args[a]) == Evaluated:
-                        try:
-                            args[index] = def_args[a].func()
-                        except:
-                            print("A positional argument with default value Evaluated was not passed.")
-                            raise
-                            
-                    elif type(def_args[a]) == Isolated:
-                        try:
-                            args[index] = copy.deepcopy(args[index])
-                        except IndexError:
-                            print("A positional argument with default value Isolated was not passed.")
-                            raise
-            
+
+                    if isinstance(spec_defaults[i], Evaluated):
+                        args_l.append(spec_defaults[i].func())
+                    elif isinstance(spec_defaults[i], Isolated):
+                        raise ValueError(
+                            "A positional argument with default value Isolated was not passed."
+                        )
+                    else:
+                        args_l.append(spec_defaults[i])
+
+                shift = len(spec_args) - len(spec_defaults)
+                for i in range(start):
+                    if isinstance(spec_defaults[i], Isolated):
+                        args_l[shift + i] = copy.deepcopy(args_l[shift + i])
+
+            args = tuple(args_l)
             if capacity <= 0:
                 return func(*args, **kwargs)
-            
             try:
-                key = hash((tuple(args), tuple(sorted(kwargs.items()))))
+                key = hash((args, tuple(sorted(kwargs.items()))))
             except TypeError:
                 return func(*args, **kwargs)
 
             if key in cache:
                 cache.move_to_end(key)
                 return cache[key]
-            
+
             if len(cache) == capacity:
                 cache.popitem(last=False)
 
@@ -98,4 +108,3 @@ def smart_args(*, capacity: int = 0, pos_args: bool = False) -> Callable[..., An
         return inner
 
     return decorator
-    
